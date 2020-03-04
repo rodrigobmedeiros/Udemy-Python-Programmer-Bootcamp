3119e257d32163dab838c1c5ea60d96097e8d4cd
Rodrigo Bernardo Medeiros <rodrimedeiros@gmail.com>
2020-03-04 11:34:45 -0300

    Varibles - exercise 3 and 4

---
+++ product_of_two_integers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 11:31:40 2020
+
+@author: DigaoSuplementos
+"""
+
+# Progran to calculate the product between two integer
+
+num_1 = int(input('Enter with the first integer number \n =====> '))
+num_2 = int(input('Enter with the second integer number \n =====> '))
+
+product = num_1 * num_2
+print()
+print('The product of', num_1, 'and', num_2, 'equals', product)
---
+++ sum_two_integers.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar  4 11:20:06 2020
+
+@author: DigaoSuplementos
+"""
+
+# Program to sum two integral numbers
+
+num_1 = int(input('Enter with the first integer number \n =====> '))
+num_2 = int(input('Enter with the second integer number \n =====> '))
+
+sum_integers = num_1 + num_2
+
+print('The sum of', num_1, 'and', num_2, 'equals', sum_integers)
